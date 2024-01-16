8da4c933049f94c3173984fafa7044e1f89078c0
Ashish Pawar <ashishpaw1234@gmail.com>
2024-01-16 10:00:36 +0530

    only two no

--- practice.py
+++ practice.py
@@ -1,7 +1,7 @@
 # list compretion
-if __name__ == '__main__':
-    x = int(input())
-    y = int(input())
-    z = int(input())
-    n = int(input())
-    print(list([i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)  if i+j+k !=n))
+x = int(input())
+y = int(input())
+
+n = int(input())
+print(list([i,j] for i in range(x+1) for j in range(y+1)   if i+j !=n))
+
